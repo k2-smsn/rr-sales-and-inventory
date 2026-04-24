ec09cdf2b542c6260bdfaeefc2c36e23b6bd0715
k2 <kkvs1805@gmail.com>
2026-04-24 22:50:55 +0800

    initial commit

---
+++ src/main/java/com/mycompany/rrsalesandinventory/RrSalesAndInventory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rrsalesandinventory;
+
+/**
+ *
+ * @author k2
+ */
+public class RrSalesAndInventory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
